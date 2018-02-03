73750f8f9d806e26074c68d1852312a791b80210
Андрей Кудёлко <a.a.kudelko@hott.local>
2018-02-03 14:53:22 +0300

    add create-react-app

---
+++ create-react-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to Create-React-App</h1>
+        </header>
+        <p>Create React apps with no build configuration</p>
+      </div>
+    );
+  }
+}
+
+export default App;
